211360f7ee8b9526acda4bcd0db076b8eb697bde
Atsushi Nakamura <nuits.jp@live.jp>
2021-01-24 08:14:29 +0900

    テストコードの名前空間を変更

--- ExpressionTreeWithCache.Test/GenerateEqualsTest.cs
+++ ExpressionTreeWithCache.Test/GenerateEqualsTest.cs
@@ -2,25 +2,22 @@ using Xunit;
 
 namespace ExpressionTreeWithCache.Test
 {
-    namespace GenerateEqualsTest
+    public class EqualsがIdentifier属性で指定されたプロパティで行われること
     {
-        public class EqualsがIdentifier属性で指定されたプロパティで行われること
+        [Fact]
+        public void Identifier属性の指定されたプロパティ名がCodeである()
         {
-            [Fact]
-            public void Identifier属性の指定されたプロパティ名がCodeである()
-            {
-                var customer1 = new Customer { Code = 1 };
-                var customer2 = new Customer { Code = 1 };
-                Assert.True(customer1.Equals(customer2));
-            }
+            var customer1 = new Customer { Code = 1 };
+            var customer2 = new Customer { Code = 1 };
+            Assert.True(customer1.Equals(customer2));
+        }
 
-            [Fact]
-            public void Identifier属性の指定されたプロパティ名がIdである()
-            {
-                var employee1 = new Employee { Id = 1 };
-                var employee2 = new Employee { Id = 1 };
-                Assert.True(employee1.Equals(employee2));
-            }
+        [Fact]
+        public void Identifier属性の指定されたプロパティ名がIdである()
+        {
+            var employee1 = new Employee { Id = 1 };
+            var employee2 = new Employee { Id = 1 };
+            Assert.True(employee1.Equals(employee2));
         }
     }
 }
--- ILGenerator.Test/GenerateEqualsTest.cs
+++ ILGenerator.Test/GenerateEqualsTest.cs
@@ -4,37 +4,34 @@ using Xunit;
 
 namespace ILGenerator.Test
 {
-    namespace GenerateEqualsTest
+    public class EqualsがIdentifier属性で指定されたプロパティで行われること
     {
-        public class EqualsがIdentifier属性で指定されたプロパティで行われること
+        private static readonly TestResult TestResult;
+
+        static EqualsがIdentifier属性で指定されたプロパティで行われること()
         {
-            private static readonly TestResult TestResult;
+            var weavingTask = new ModuleWeaver();
+            TestResult = weavingTask.ExecuteTestRun("ILGenerator.dll", false);
+        }
 
-            static EqualsがIdentifier属性で指定されたプロパティで行われること()
-            {
-                var weavingTask = new ModuleWeaver();
-                TestResult = weavingTask.ExecuteTestRun("ILGenerator.dll", false);
-            }
+        [Fact]
+        public void Identifier属性の指定されたプロパティ名がCodeである()
+        {
+            var customer1 = TestResult.GetInstance("ILGenerator.Customer");
+            customer1.Code = 1;
+            var customer2 = TestResult.GetInstance("ILGenerator.Customer");
+            customer2.Code = 1;
+            Assert.True(customer1.Equals(customer2));
+        }
 
-            [Fact]
-            public void Identifier属性の指定されたプロパティ名がCodeである()
-            {
-                var customer1 = TestResult.GetInstance("ILGenerator.Customer");
-                customer1.Code = 1;
-                var customer2 = TestResult.GetInstance("ILGenerator.Customer");
-                customer2.Code = 1;
-                Assert.True(customer1.Equals(customer2));
-            }
-
-            [Fact]
-            public void Identifier属性の指定されたプロパティ名がIdである()
-            {
-                var employee1 = TestResult.GetInstance("ILGenerator.Employee");
-                employee1.Id = 1;
-                var employee2 = TestResult.GetInstance("ILGenerator.Employee");
-                employee2.Id = 1;
-                Assert.True(employee1.Equals(employee2));
-            }
+        [Fact]
+        public void Identifier属性の指定されたプロパティ名がIdである()
+        {
+            var employee1 = TestResult.GetInstance("ILGenerator.Employee");
+            employee1.Id = 1;
+            var employee2 = TestResult.GetInstance("ILGenerator.Employee");
+            employee2.Id = 1;
+            Assert.True(employee1.Equals(employee2));
         }
     }
 }
