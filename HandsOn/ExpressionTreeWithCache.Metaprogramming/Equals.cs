b74306c28daec2e37836844377c05d902e8e4dbf
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2021-02-19 06:06:31 +0900

    ExpressionTreeWithCache.Metaprogrammingをキャッシング適用前に修正

--- HandsOn/ExpressionTreeWithCache.Metaprogramming/Equals.cs
+++ HandsOn/ExpressionTreeWithCache.Metaprogramming/Equals.cs
@@ -8,33 +8,22 @@ namespace ExpressionTreeWithCache.Metaprogramming
 {
     public class Equals<T>
     {
-        public static Equals<T> Instance = new();
-
-        private readonly Func<T, int> _getIdentify;
-
-        private Equals()
-        {
-            var getIdentifyMethod = typeof(T)
-                .GetProperties()
-                .Single(x => x.GetCustomAttribute<IdentifierAttribute>() is not null)
-                .GetGetMethod();
-
-            var entity = Expression.Parameter(typeof(T));
-            var getterCall = Expression.Call(entity, getIdentifyMethod);
-            var lambda = Expression.Lambda(getterCall, entity);
-
-            var getIdentify = (Func<T, int>)lambda.Compile();
-
-            _getIdentify = getIdentify;
-        }
-
-        public static bool Invoke(T self, object other) => Instance.InvokeInner(self, other);
-
-        private bool InvokeInner(T self, object other)
+        public static bool Invoke(T self, object other)
         {
             if (other is T t)
             {
-                return _getIdentify(self).Equals(_getIdentify(t));
+                var getIdentifyMethod = typeof(T)
+                    .GetProperties()
+                    .Single(x => x.GetCustomAttribute<IdentifierAttribute>() is not null)
+                    .GetGetMethod();
+
+                var entity = Expression.Parameter(typeof(T));
+                var getterCall = Expression.Call(entity, getIdentifyMethod);
+                var lambda = Expression.Lambda(getterCall, entity);
+
+                var getIdentify = (Func<T, int>)lambda.Compile();
+
+                return getIdentify(self).Equals(getIdentify(t));
             }
 
             return false;
