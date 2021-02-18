3d20a5da10ed527b3b4d43241a33a7133190feea
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2021-02-19 06:05:23 +0900

    ReflectionWithCache.Metaprogrammingをキャッシュ適用前の状態に変更

--- HandsOn/ReflectionWithCache.Metaprogramming/Equals.cs
+++ HandsOn/ReflectionWithCache.Metaprogramming/Equals.cs
@@ -6,25 +6,15 @@ namespace ReflectionWithCache.Metaprogramming
 {
     public class Equals<T>
     {
-        public static Equals<T> Instance = new();
-
-        private readonly PropertyInfo _propertyInfo;
-
-
-        private Equals()
-        {
-            _propertyInfo = typeof(T)
-                .GetProperties()
-                .Single(x => x.GetCustomAttribute<IdentifierAttribute>() is not null);
-        }
-
-        public static bool Invoke(T self, object other) => Instance.InvokeInner(self, other);
-
-        private bool InvokeInner(T self, object other)
+        public static bool Invoke(T self, object other)
         {
             if (other is T t)
             {
-                return Equals(_propertyInfo.GetValue(self), _propertyInfo.GetValue(t));
+                var identifyProperty = typeof(T)
+                    .GetProperties()
+                    .Single(x => x.GetCustomAttribute<IdentifierAttribute>() is not null);
+
+                return Equals(identifyProperty.GetValue(self), identifyProperty.GetValue(t));
             }
 
             return false;
